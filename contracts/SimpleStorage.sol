f62cfab98290fd00090841fdab5192f1a0c64fb6
aforanup <aforanupn@gmail.com>
2024-11-30 23:20:42 +0545

    added a struct and array function

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,13 +2,24 @@
 pragma solidity ^0.8.7;
 
 contract SimpleStorage {
+    struct People {
+        uint favouriteNumber;
+        string name;
+    }
+
     uint public favouriteNumber;
+
+    People[] public people;
 
     function store(uint _number) public {
         favouriteNumber = _number;
     }
 
-    function get() public view returns(uint){
+    function get() public view returns (uint) {
         return favouriteNumber;
+    }
+
+    function addPerson(uint _number, string memory _name) public {
+        people.push(People(_number, _name));
     }
 }
