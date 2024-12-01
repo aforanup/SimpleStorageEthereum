f11e0021dd0af42beeb17946df32da0f132f2056
aforanup <aforanupn@gmail.com>
2024-12-01 18:19:49 +0545

    added mapping function

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -10,6 +10,7 @@ contract SimpleStorage {
     uint public favouriteNumber;
 
     People[] public people;
+    mapping(string => uint) public nameToNum;
 
     function store(uint _number) public {
         favouriteNumber = _number;
@@ -21,5 +22,6 @@ contract SimpleStorage {
 
     function addPerson(uint _number, string memory _name) public {
         people.push(People(_number, _name));
+        nameToNum[_name] = _number;
     }
 }
