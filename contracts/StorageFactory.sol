c068a8074a79e47a1a19c851235bf96f36150103
aforanup <aforanupn@gmail.com>
2024-12-18 16:58:05 +0545

    created set of functions with simplestorage importing it and using it to store

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.5.16;
 
 import "./SimpleStorage.sol";
 
-contract StorageFactory {}
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorage() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint _index, uint _num) public {
+        SimpleStorage sfAddress = simpleStorageArray[_index];
+        sfAddress.store(_num);
+    }
+
+    function sfGet(uint _index) public view returns (uint) {
+        SimpleStorage sfAddress = simpleStorageArray[_index];
+        return sfAddress.get();
+    }
+}
